cf3bcc9e645c8c05fb555751b4f24efa29376879
Gabe571 <gabriel.castro1028@gmail.com>
2021-01-12 11:57:19 -0800

    getting class specs

---
+++ src/Api.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react'
+
+export default function Api() {
+
+  return (
+  <div>
+   </div> //map over JSON
+   )
+  }
+
+// client id 4d7685154d8e4ae7911f4506f179d45f
+// secret ND43xZowRLxrgOLWzQBPVeH26S6F2iFE
--- src/App.js
+++ src/App.js
@@ -4,24 +4,43 @@ export default function App() {
 
   const [races, setRaces] = useState('races')
   const [items, setItems] = useState([])
+  const [specializations, setSpecializations] = useState('specialization')
+  const [specs, setSpecs] = useState([])
   
+
   useEffect(() => {
   fetch(`https://us.api.blizzard.com/data/wow/playable-race/index?namespace=static-us&locale=en_US&access_token=US02EuGg12ws0BgdyhjHJsnTRMWXsZNHIw`)
   .then(response => response.json())
   .then(json => setItems(json.races))
   },[races]) 
-  console.log(items)
-  //any value change run useEffect hook
+
+
+  useEffect(() => {
+    fetch(`https://us.api.blizzard.com/data/wow/playable-specialization/index?namespace=static-us&locale=en_US&access_token=US02EuGg12ws0BgdyhjHJsnTRMWXsZNHIw`)
+    .then(response => response.json())
+    .then(json => setSpecs(json.character_specializations))
+    },[specializations]) 
+  
   return (
   <div>
+  <div>
   {items.map(item => {
- return <li key={item.id}>
+ return <pre key={item.id}>
   Race: {item.name}
-</li>
+</pre>
 })}
-   </div> //map over JSON
+</div>
+<div>
+  {specs.map(spec => {
+ return <pre key={spec.id}>
+  Specialization: {spec.name}
+</pre>
+})}
+</div>
+   </div> 
    )
   }
 
 // client id 4d7685154d8e4ae7911f4506f179d45f
 // secret ND43xZowRLxrgOLWzQBPVeH26S6F2iFE
+
