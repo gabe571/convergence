607a1e00934041119d842b24539433351b3413d7
Gabe571 <gabriel.castro1028@gmail.com>
2021-01-12 11:29:25 -0800

    working race api call

--- src/App.js
+++ src/App.js
@@ -7,21 +7,21 @@ export default function App() {
   
   useEffect(() => {
   fetch(`https://us.api.blizzard.com/data/wow/playable-race/index?namespace=static-us&locale=en_US&access_token=US02EuGg12ws0BgdyhjHJsnTRMWXsZNHIw`)
-  .then(response => response.races.json())
-  .then(console.log())
+  .then(response => response.json())
+  .then(json => setItems(json.races))
   },[races]) 
- 
-
+  console.log(items)
+  //any value change run useEffect hook
   return (
-    <div>
-    {items.map(item => {
-   <li key={item.id}>
-    Race: {item.name}
-  </li>
-  })}
-     </div> 
-     )
-    }
+  <div>
+  {items.map(item => {
+ return <li key={item.id}>
+  Race: {item.name}
+</li>
+})}
+   </div> //map over JSON
+   )
+  }
 
 // client id 4d7685154d8e4ae7911f4506f179d45f
 // secret ND43xZowRLxrgOLWzQBPVeH26S6F2iFE
---
+++ src/App1.js
@@ -0,0 +1,27 @@
+// import React, { useState, useEffect } from 'react'
+
+// export default function App() {
+
+//   const [races, setRaces] = useState('races')
+//   const [items, setItems] = useState([])
+  
+//   useEffect(() => {
+//   fetch(`https://us.api.blizzard.com/data/wow/playable-race/index?namespace=static-us&locale=en_US&access_token=US02EuGg12ws0BgdyhjHJsnTRMWXsZNHIw`)
+//   .then(response => response.races.json())
+//   .then(console.log())
+//   },[races]) 
+ 
+
+//   return (
+//     <div>
+//     {items.map(item => {
+//    <li key={item.id}>
+//     Race: {item.name}
+//   </li>
+//   })}
+//      </div> 
+//      )
+//     }
+
+// client id 4d7685154d8e4ae7911f4506f179d45f
+// secret ND43xZowRLxrgOLWzQBPVeH26S6F2iFE
