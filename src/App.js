5b82f1f4984ed188ec4d3c37c3e0d75020a77872
Gabe571 <gabriel.castro1028@gmail.com>
2021-01-12 14:05:37 -0800

    cleaned up flies, added title

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 
+
 export default function App() {
 
   const [races, setRaces] = useState('races')
@@ -54,35 +55,3 @@ export default function App() {
    </div> 
    )
   }
-
-
-// client id 4d7685154d8e4ae7911f4506f179d45f
-// secret ND43xZowRLxrgOLWzQBPVeH26S6F2iFE
-
-
-// import React, { useState, useEffect } from 'react'
-
-// export default function App() {
-
-//   const [classes, setClasses] = useState('classes')
-//   const [className, setClassNames] = useState([])
-
-//    useEffect(() => {
-//       fetch(`https://us.api.blizzard.com/data/wow/playable-class/index?namespace=static-us&locale=en_US&access_token=US02EuGg12ws0BgdyhjHJsnTRMWXsZNHIw`)
-//       .then(response => response.json())
-//       .then(json => setClassNames(json.classes))
-//       },[classes]) 
-
-//   return (
-//   <div>
-//  <div>
-// {className.map(item => {
-// return <pre key={item.id}>
-//  Class: {item.name}
-//  </pre>
-//  })}
-//   </div>
-//    </div> 
-//    )
-//   }
-
