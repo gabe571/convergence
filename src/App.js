f1b3dcd2a2632fb22757e662ccc3992b4da68c17
Gabe571 <gabriel.castro1028@gmail.com>
2021-01-12 10:44:33 -0800

    working thru API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function App() {
-  const [resourceType, SetResourceType] = useState('posts')
+
+  const [races, setRaces] = useState('races')
+  const [items, setItems] = useState([])
+  
+  useEffect(() => {
+  fetch(`https://us.api.blizzard.com/data/wow/playable-race/index?namespace=static-us&locale=en_US&access_token=US02EuGg12ws0BgdyhjHJsnTRMWXsZNHIw`)
+  .then(response => response.races.json())
+  .then(console.log())
+  },[races]) 
+ 
 
   return (
     <div>
-      <button onClick={() => SetResourceType('posts')}>Posts</button>
-      <button onClick={() => SetResourceType('users')}>Users</button>
-      <button onClick={() => SetResourceType('Comments')}>Comments</button>
-      <h1>{resourceType}</h1>
-    </div>
-  )
-}
+    {items.map(item => {
+   <li key={item.id}>
+    Race: {item.name}
+  </li>
+  })}
+     </div> 
+     )
+    }
+
+// client id 4d7685154d8e4ae7911f4506f179d45f
+// secret ND43xZowRLxrgOLWzQBPVeH26S6F2iFE
