e1317076ea4a1ed284861fb784d997f2571f92ed
Gabe571 <gabriel.castro1028@gmail.com>
2021-01-13 15:13:03 -0800

    RAWG APP.JS

--- src/Api.jsx
+++ src/Api.jsx
@@ -7,6 +7,3 @@ export default function Api(){
         </div>
     )
 }
-// client id 4d7685154d8e4ae7911f4506f179d45f
-// secret ND43xZowRLxrgOLWzQBPVeH26S6F2iFE
-
--- src/App.js
+++ src/App.js
@@ -1,57 +1,22 @@
-import React, { useState, useEffect } from 'react'
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import TopGames from './TopGames';
+import Search from '.Nav';
+import Nav from "./Nav";
+import GameDetail from "./GameDetail";
 
 
 export default function App() {
 
-  const [races, setRaces] = useState('races')
-  const [items, setItems] = useState([])
-  const [classes, setClasses] = useState('classes')
-  const [className, setClassNames] = useState([])
-  const [specializations, setSpecializations] = useState('specializations')
-  const [specs, setSpecs] = useState([])
- 
-
-  useEffect(() => {
-  fetch(`https://us.api.blizzard.com/data/wow/playable-race/index?namespace=static-us&locale=en_US&access_token=US02EuGg12ws0BgdyhjHJsnTRMWXsZNHIw`)
-  .then(response => response.json())
-  .then(json => setItems(json.races))
-  },[races]) 
-
-   useEffect(() => {
-      fetch(`https://us.api.blizzard.com/data/wow/playable-class/index?namespace=static-us&locale=en_US&access_token=US02EuGg12ws0BgdyhjHJsnTRMWXsZNHIw`)
-      .then(response => response.json())
-      .then(json => setClassNames(json.classes))
-      },[classes]) 
-
-  useEffect(() => {
-    fetch(`https://us.api.blizzard.com/data/wow/playable-specialization/index?namespace=static-us&locale=en_US&access_token=US02EuGg12ws0BgdyhjHJsnTRMWXsZNHIw`)
-    .then(response => response.json())
-    .then(json => setSpecs(json.character_specializations))
-    },[specializations])
-    
   return (
-  <div>
-   <div>
-  {items.map(item => {
- return <pre key={item.id}>
-  Race: {item.name}
-</pre>
-})}
- </div>
- <div>
-  {className.map(cl => {
- return <pre key={cl.id}>
-  Class: {cl.name}
-</pre>
-})}
- </div>
-<div>
-  {specs.map(spec => {
- return <pre key={spec.id}>
-  Specialization: {spec.name}
-</pre>
-})}
-</div>
-   </div> 
-   )
-  }
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+        <Route exact path='/topgames' component={TopGames} />
+        <Route exact path='/search' component={Search} />
+        <Route exact path='/game/:name' component={GameDetail} />
+        </Switch>
+      </div>
+    </Router>
+  )
+}
