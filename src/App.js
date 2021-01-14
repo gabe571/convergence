ce78b31f65b4d80e432a6b11b9493e6f88e107c7
Gabe571 <gabriel.castro1028@gmail.com>
2021-01-13 16:16:45 -0800

    working skeleton

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
+import './App.css';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import TopGames from './TopGames';
-import Search from '.Nav';
+import TopGames from "./TopGames";
+import Search from "./Search";
 import Nav from "./Nav";
 import GameDetail from "./GameDetail";
 
-
-export default function App() {
-
+function App() {
+  
   return (
     <Router>
       <div className="App">
         <Nav />
         <Switch>
-        <Route exact path='/topgames' component={TopGames} />
-        <Route exact path='/search' component={Search} />
-        <Route exact path='/game/:name' component={GameDetail} />
+          <Route exact path='/topgames' component={TopGames} />
+          <Route exact path='/search' component={Search} />
+          <Route path='/game/:name' component={GameDetail} />
         </Switch>
       </div>
     </Router>
-  )
+  );
 }
+
+export default App;
