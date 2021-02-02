856bb88c63138fe246ed8116c181136fdd7c1a71
Gabe571 <gabriel.castro1028@gmail.com>
2021-02-02 14:32:00 -0800

    Reviews Template

---
+++ src/Reviews.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Reviews = () => {
+    return (
+ <div className="review-container">
+     <div className="review-card">
+
+     </div>
+    </div>
+    )
+}
+
+export default Reviews;
