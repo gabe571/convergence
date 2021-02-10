e06e9b3c60fb4c9363e20f75a0267e8e70e18217
Gabe571 <gabriel.castro1028@gmail.com>
2021-02-10 13:08:16 -0800

    working avatar

--- src/Review.jsx
+++ src/Review.jsx
@@ -4,9 +4,11 @@ import React from 'react'
 const Reviews = (props) => {
 
     const { review } = props
+    console.log(props)
     return (
  <div className="review-container">
      <div className="review-card">
+     <img src='../avatar.png' className="avatar" width="50" height="50" alt='avatar'/>
       <div className="review-username">{review.user_username}</div>
          <div className="review-content">{review.reviewed_game}</div>
             <div className="review-rating">{review.rating}<span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span></div>
